6a28ef387490f1592eb9ec77696bdd22684573a9
ericpcho <ericpcho@gmail.com>
2017-09-20 20:30:08 -0700

    added tests for put delete and post

--- test/test.js
+++ test/test.js
@@ -25,13 +25,16 @@ function seedPost() {
   };
 }
 
+let randomId;
+
 function seedData() {
   console.log('seeding data');
   const seedDataArray = [];
   for (let i = 0; i < 10 ; i++) {
     seedDataArray.push(seedPost());
   }
-  return BlogPost.insertMany(seedDataArray);
+  return BlogPost.insertMany(seedDataArray)
+  .then(function(res){randomId = res[0].id, console.log(res[0].id)})
 }
 
 describe('Blog Post API', function() {
@@ -40,14 +43,15 @@ describe('Blog Post API', function() {
   });
 
   beforeEach(function () {
-    if ( mongoose.connection.collections['blogposts']) {
-      mongoose.connection.collections['blogposts'].drop(      function(err) {
+    if (mongoose.connection.collections['blogposts']) {
+      new Promise ((resolve, reject) => {mongoose.connection.collections['blogposts'].drop(function(err) {
         console.log('collection dropped');
-      });
+      })})
+      .then(); 
     } else {
-      console.log('nothing to drop');      
-    }
-    return seedData();    
+      console.log('nothing to drop');     
+    } 
+    return seedData();  
   });
 
   afterEach(function () {
@@ -59,8 +63,6 @@ describe('Blog Post API', function() {
   });
 
   describe('Start Test Suite', function() {
-
-    let testId;
     
     it('should return all blog posts', function() {
       return chai.request(app)
@@ -74,49 +76,94 @@ describe('Blog Post API', function() {
           res.body.forEach(function(post) {
             post.should.be.an('object'); // each item in array type is object
             post.should.include.keys(['id', 'content', 'author','title', 'created']); // each object in array keys id, author, content, title, created
-          });
-          testId = res.body[0].id;          
+          });        
         });
     });
 
     it('should return the 1st item in db', function() {
       return chai.request(app)
-        .get(`/posts/${testId}`)
+        .get(`/posts/${randomId}`)
         .then(function(res){
           res.should.be.json; 
           res.should.have.status(200); 
-          res.body.should.be.an('array'); 
-          res.body.should.have.length.equal(1);
-          res.body.forEach(function(post) {
-            post.should.be.an('object'); 
-            post.should.include.keys(['id', 'content', 'author','title', 'created']); 
-            post.body.id.should.equal(testId);
-          });
+          res.body.should.be.an('object'); 
+          res.body.should.include.keys(['id', 'content', 'author','title', 'created']); 
+          res.body.id.should.equal(randomId);
         });
     });
 
-    it.skip('should something', function() {
+    it('should post an item to the db', function() {
+      const testPostData = {
+        author: {
+          firstName: faker.Name.firstName(),
+          lastName: faker.Name.lastName()
+        },
+        title: faker.Lorem.sentence(5),
+        content: faker.Lorem.paragraph()
+      }
+
       return chai.request(app)
         .post('/posts') 
+        .send(testPostData)
         .then(function(res){
-          res.should
-        });
+          res.should.be.json;
+          res.should.have.status(201);
+          res.body.should.be.an('object');
+          res.body.should.include.keys(['id', 'content', 'author','title', 'created']);
+          res.body.id.should.not.be.null;
+          res.body.author.should.equal(
+            `${testPostData.author.firstName} ${testPostData.author.lastName}`);
+            return BlogPost.findById(res.body.id);
+          })
+          .then(function(post) {
+            post.title.should.equal(testPostData.title);
+            post.content.should.equal(testPostData.content);
+            post.author.firstName.should.equal(testPostData.author.firstName);
+            post.author.lastName.should.equal(testPostData.author.lastName);
+          });
     });
 
-    it.skip('should something', function() {
+    it('should delete a post with a specific id', function() {
       return chai.request(app)
-        .delete('/posts/:id')
+        .delete(`/posts/${randomId}`)
         .then(function(res){
-          res.should
-        });
+          res.should.have.status(204);
+          return BlogPost.findById(`${randomId}`)
+        })
+        .then(post => {
+          should.not.exist(post)
+        })
     });
 
-    it.skip('should something', function() {
+    it('should udpate a post with a specific id', function() {
+
+      const testUpdateData = {
+        author: {
+          firstName: faker.Name.firstName(),
+          lastName: faker.Name.lastName()
+        },
+        title: faker.Lorem.sentence(5),
+        content: faker.Lorem.paragraph()
+      }
+
+      return BlogPost.findOne()
+      .then(post => {
+        testUpdateData.id = post.id
+
       return chai.request(app)
-        .put('/posts/:id') 
+        .put(`/posts/${randomId}`) 
+        .send(testUpdateData)
+      })
         .then(function(res){
-          res.should
-        });
+          res.should.have.status(204);
+          return BlogPost.findById(testUpdateData.id)
+        })
+        .then( post => {
+          post.title.should.equal(testUpdateData.title);
+          post.content.should.equal(testUpdateData.content);
+          post.author.firstName.should.equal(testUpdateData.author.firstName);
+          post.author.lastName.should.equal(testUpdateData.author.lastName);
+        })
     });
 
   });
